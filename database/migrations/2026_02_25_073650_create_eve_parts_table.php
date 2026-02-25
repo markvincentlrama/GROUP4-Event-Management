f1e4a425ff471f048031bce798af4d347b343d4f
IDDUJUSTIN <iddurutjustin23@gmail.com>
2026-02-25 15:46:56 +0800

    fix typo

--- database/migrations/2026_02_25_073650_create_eve_parts_table.php
+++ database/migrations/2026_02_25_073650_create_eve_parts_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('eve_parts');
+        Schema::dropIfExists('event_participant');
     }
 };
