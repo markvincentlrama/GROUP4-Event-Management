7f0740fcd70e87880248738559316bde7d8c5029
IDDUJUSTIN <iddurutjustin23@gmail.com>
2026-02-25 15:40:32 +0800

    Data Model For Events and Participation

---
+++ app/Models/Events.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'event_name',
+        'category',
+        'event_date',
+        'location',
+    ];
+
+}
