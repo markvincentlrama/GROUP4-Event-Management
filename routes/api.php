4bc8b52a1d85f3fbdf1911907890e7880000b2b5
markvincentlrama <markvincent.l.rama@isu.edu.ph>
2026-03-04 15:41:29 +0800

    Added missing import of EventStatController

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventsController;
+use App\Http\Controllers\EventStatsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
